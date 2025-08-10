e68e4e3c03bb3e58a291da674969137960693153
ddigloria <ddigloria@emasphere.com>
2025-08-10 20:24:15 +0200

    wip

--- src/app/components/projection/scoreboard/scoreboard-team/scoreboard-team.component.ts
+++ src/app/components/projection/scoreboard/scoreboard-team/scoreboard-team.component.ts
@@ -1,6 +1,7 @@
 import {Component, input} from '@angular/core';
 import {NgClass, NgTemplateOutlet} from "@angular/common";
 import { TeamNumber } from '@enums/team-number.enum'
+import { Team } from '@models/team';
 
 @Component({
   selector: 'app-scoreboard-team',
@@ -16,7 +17,8 @@ export class ScoreboardTeamComponent {
 
   maxFouls = Array(3);
 
-  team = input<TeamNumber>(TeamNumber.TEAM_A);
+  teamNumber = input<TeamNumber>(TeamNumber.TEAM_A);
+  team = input.required<Team>();
   fouls = input<number>(0);
   score = input<number>(0);
 
--- src/app/components/projection/scoreboard/scoreboard.component.ts
+++ src/app/components/projection/scoreboard/scoreboard.component.ts
@@ -1,6 +1,8 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, ResourceRef, signal} from '@angular/core';
 import {ScoreboardTeamComponent} from "./scoreboard-team/scoreboard-team.component";
 import {TeamNumber} from "@enums/team-number.enum";
+import {ImproDataService} from "@services/impro-data.service";
+import {GameData} from "@models/game-data";
 @Component({
   selector: 'app-scoreboard',
   imports: [
@@ -12,7 +14,12 @@ import {TeamNumber} from "@enums/team-number.enum";
 export class ScoreboardComponent {
   readonly Team = TeamNumber;
 
+  private _improDataService = inject(ImproDataService);
+
   themeTitle = signal('POURVU QUE CA MARCHE !');
   themeStyle = signal('A la manière de la chanson de Gold');
+
+
+  gameData = this._improDataService.gameData;
 
 }
